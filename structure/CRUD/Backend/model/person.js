c77f0489496d93ef68c0d0b45df5c12bd81c5fb8
Sainath Gopinath <sainath.gopinath@gmail.com>
2020-03-26 00:13:20 -0400

    Updated with AES

--- structure/CRUD/Backend/model/person.js
+++ structure/CRUD/Backend/model/person.js
@@ -1,8 +1,41 @@
 const mongoose = require('mongoose');
+//var bcrypt = require('bcrypt');
+//var SALT_WORK_FACTOR = 10;
 
+/* My modifications */
+
+var Schema = mongoose.Schema;
+var mySchema = new Schema({
+    name:{type : String},
+    mail:{type : String},
+    class:{type : Number}
+});
+ 
+var Person = mongoose.model('Person', mySchema);
+ 
+/* mySchema.pre('save', function(next){
+    var user = this;
+    if (!user.isModified('name')) return next();
+ 
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+        if(err) return next(err);
+ 
+        bcrypt.hash(user.name, salt, function(err, hash){
+            if(err) return next(err);
+ 
+            user.name = hash;
+            
+            next();
+        });
+    });
+});
+*/
+
+/* Original program
 var Person = mongoose.model('Person',{
     name:{type : String},
     mail:{type : String},
     class:{type : Number}
-})
+}) */
+
  module.exports = {Person}
