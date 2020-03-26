6ade1b9d9375ae12caed48b12429adb464e131b6
Sainath Gopinath <sainath.gopinath@gmail.com>
2020-03-26 00:12:18 -0400

    Backend files updated with AES Encryption

--- structure/CRUD/Backend/controllers/personController.js
+++ structure/CRUD/Backend/controllers/personController.js
@@ -21,10 +21,15 @@ router.post('/',(req,res) => {
         class : req.body.class
     });
     Person.findOne({'mail':req.body.mail}, (err, docs) => {
+        per.name = encrypt(per.name)
+        per.mail = encrypt(per.mail)
+        //per.class = encrypt(per.class.toString())
         if(!docs) {
             per.save((err, doc) => {
-                if(!err){
+                if(!err){                    
                     res.status(200).send({ auth: true, doc, message:"1 documents inserted!" });
+                    console.log("encrypted: ", doc.name, "decrypted: ", decrypt(encryptedText[0]))
+                    console.log("encrypted: ", doc.mail, "decrypted: ", decrypt(encryptedText[1]))
                 }
                 else { console.log('Error in user inserting data' + JSON.stringify(err, undefined, 2)); }
             });
@@ -76,3 +81,35 @@ router.delete('/:id',(req,res) => {
 })
 
 module.exports = router
+
+
+// My Trial
+// Nodejs encryption with CTR
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+var encryptedText = new Array(2);
+encryptedText[0] = 0, encryptedText[1] = 0
+
+function encrypt(text) {
+ let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+ let encrypted = cipher.update(text);
+ encrypted = Buffer.concat([encrypted, cipher.final()]);
+ //return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+ if (encryptedText[0]==0){
+ encryptedText[0] = { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') }
+ }
+ else {encryptedText[1] = { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex')}
+}
+ return encrypted.toString('hex');
+}
+
+function decrypt(text) {
+ let iv = Buffer.from(text.iv, 'hex');
+ let encryptedText = Buffer.from(text.encryptedData, 'hex');
+ let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+ let decrypted = decipher.update(encryptedText);
+ decrypted = Buffer.concat([decrypted, decipher.final()]);
+ return decrypted.toString();
+}
